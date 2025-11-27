0920999f32552e45a074b79fb625ead80960615c
Caleb Irwin <me@calebirwin.ca>
2025-11-27 17:28:57 -0500

    make it so sidebar works in app builder pattern

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -12,6 +12,7 @@ import interface_adapter.login.LoginViewModel;
 import interface_adapter.logout.LogoutController;
 import interface_adapter.logout.LogoutPresenter;
 import interface_adapter.navigation.NavigationController;
+import interface_adapter.navigation.NavigationPresenter;
 import interface_adapter.review_flashcards.ReviewFlashCardsController;
 import interface_adapter.review_flashcards.ReviewFlashCardsPresenter;
 import interface_adapter.review_flashcards.ReviewFlashCardsViewModel;
@@ -35,11 +36,6 @@ import use_case.signup.SignupInputBoundary;
 import use_case.signup.SignupInteractor;
 import use_case.signup.SignupOutputBoundary;
 import view.*;
-import view.LoggedInView;
-import view.LoginView;
-import view.ReviewFlashCardsView;
-import view.SignupView;
-import view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -54,182 +50,191 @@ import use_case.create_flashcard.CreateFlashcardInputBoundary;
 import use_case.create_flashcard.CreateFlashcardInteractor;
 
 public class AppBuilder {
-        private final JPanel cardPanel = new JPanel();
-        private final CardLayout cardLayout = new CardLayout();
-        final UserFactory userFactory = new UserFactory();
-        final ViewManagerModel viewManagerModel = new ViewManagerModel();
-        ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
+    private final JPanel cardPanel = new JPanel();
+    private final CardLayout cardLayout = new CardLayout();
+    final UserFactory userFactory = new UserFactory();
+    final ViewManagerModel viewManagerModel = new ViewManagerModel();
+    ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
 
-        // set which data access implementation to use, can be any
-        // of the classes from the data_access package
+    // set which data access implementation to use, can be any
+    // of the classes from the data_access package
 
-        // DAO version using local file storage
-        final FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("users.csv", userFactory);
+    // DAO version using local file storage
+    final FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("users.csv", userFactory);
 
-        // DAO version using a shared external database
-        // final DBUserDataAccessObject userDataAccessObject = new
-        // DBUserDataAccessObject(userFactory);
+    // DAO version using a shared external database
+    // final DBUserDataAccessObject userDataAccessObject = new
+    // DBUserDataAccessObject(userFactory);
 
-        private SignupView signupView;
-        private SignupViewModel signupViewModel;
-        private LoginViewModel loginViewModel;
-        private LoggedInViewModel loggedInViewModel;
-        private LoggedInView loggedInView;
-        private LoginView loginView;
-        private SidebarView sidebarView;
-        private HomePage homePage;
+    private SignupView signupView;
+    private SignupViewModel signupViewModel;
+    private LoginViewModel loginViewModel;
+    private LoggedInViewModel loggedInViewModel;
+    private LoggedInView loggedInView;
+    private LoginView loginView;
+    private SidebarView sidebarView;
+    private HomePage homePage;
 
-        private ReviewFlashCardsViewModel reviewFlashCardsViewModel;
-        private ReviewFlashCardsView reviewFlashCardsView;
-        private ReviewFlashCardsController reviewFlashCardsController;
+    private ReviewFlashCardsViewModel reviewFlashCardsViewModel;
+    private ReviewFlashCardsView reviewFlashCardsView;
+    private ReviewFlashCardsController reviewFlashCardsController;
 
-        public AppBuilder() {
-                cardPanel.setLayout(cardLayout);
-        }
+    public AppBuilder() {
+        cardPanel.setLayout(cardLayout);
+    }
 
-        public AppBuilder setSidebar(NavigationController controller) {
-                this.sidebarView = new SidebarView(controller);
+    public AppBuilder addSidebar() {
+        // Create Presenter
+        NavigationOutputBoundary presenter = new NavigationPresenter(viewManagerModel);
 
-                JPanel mainPanel = new JPanel(new BorderLayout());
+        // Create Interactor
+        NavigationInputBoundary interactor = new NavigationInteractor(presenter);
 
-                mainPanel.add(this.sidebarView, BorderLayout.WEST);
-                mainPanel.add(this.cardPanel, BorderLayout.CENTER);
-                this.cardPanel.setLayout(new BorderLayout());
-                this.cardPanel.add(mainPanel);
-                return this;
-        }
+        // Create Controller
+        NavigationController navigationController = new NavigationController(interactor);
+        this.sidebarView = new SidebarView(navigationController);
+        return this;
+    }
 
-        public AppBuilder addSignupView() {
-                signupViewModel = new SignupViewModel();
-                signupView = new SignupView(signupViewModel);
-                cardPanel.add(signupView, signupView.getViewName());
-                return this;
-        }
+    public AppBuilder addSignupView() {
+        signupViewModel = new SignupViewModel();
+        signupView = new SignupView(signupViewModel);
+        cardPanel.add(signupView, signupView.getViewName());
+        return this;
+    }
 
-        public AppBuilder addReviewFlashCardsView() {
-                reviewFlashCardsViewModel = new ReviewFlashCardsViewModel();
-                // TODO: Data loading logic
-                reviewFlashCardsViewModel.setState(ReviewFlashCardsView.generateMockViewModel().getState());
+    public AppBuilder addReviewFlashCardsView() {
+        reviewFlashCardsViewModel = new ReviewFlashCardsViewModel();
+        // TODO: Data loading logic
+        reviewFlashCardsViewModel.setState(ReviewFlashCardsView.generateMockViewModel().getState());
 
-                ReviewFlashCardsPresenter reviewFlashCardsPresenter = new ReviewFlashCardsPresenter(
-                                reviewFlashCardsViewModel);
-                ReviewFlashCardsInteractor reviewFlashCardsInteractor = new ReviewFlashCardsInteractor(
-                                reviewFlashCardsPresenter);
-                reviewFlashCardsController = new ReviewFlashCardsController(reviewFlashCardsInteractor);
-                reviewFlashCardsView = new ReviewFlashCardsView(reviewFlashCardsViewModel, reviewFlashCardsController);
-                cardPanel.add(reviewFlashCardsView, reviewFlashCardsView.getViewName());
-                return this;
-        }
+        ReviewFlashCardsPresenter reviewFlashCardsPresenter = new ReviewFlashCardsPresenter(
+                reviewFlashCardsViewModel);
+        ReviewFlashCardsInteractor reviewFlashCardsInteractor = new ReviewFlashCardsInteractor(
+                reviewFlashCardsPresenter);
+        reviewFlashCardsController = new ReviewFlashCardsController(reviewFlashCardsInteractor);
+        reviewFlashCardsView = new ReviewFlashCardsView(reviewFlashCardsViewModel, reviewFlashCardsController);
+        cardPanel.add(reviewFlashCardsView, reviewFlashCardsView.getViewName());
+        return this;
+    }
 
-        public JFrame buildCreateFlashcardUI() {
+    public JFrame buildCreateFlashcardUI() {
 
-                CreateFlashcardDataAccessInterface dataAccess = new JsonFlashcardSetDataAccessObject();
+        CreateFlashcardDataAccessInterface dataAccess = new JsonFlashcardSetDataAccessObject();
 
-                CreateFlashcardPresenter presenter = new CreateFlashcardPresenter();
+        CreateFlashcardPresenter presenter = new CreateFlashcardPresenter();
 
-                CreateFlashcardInputBoundary interactor = new CreateFlashcardInteractor(dataAccess);
+        CreateFlashcardInputBoundary interactor = new CreateFlashcardInteractor(dataAccess);
 
-                CreateFlashcardController controller = new CreateFlashcardController(interactor);
+        CreateFlashcardController controller = new CreateFlashcardController(interactor);
 
-                CreateFlashcardView view = new CreateFlashcardView(controller);
+        CreateFlashcardView view = new CreateFlashcardView(controller);
 
-                JFrame frame = new JFrame("Create Flashcard");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(500, 600);
-                frame.add(view);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
+        JFrame frame = new JFrame("Create Flashcard");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 600);
+        frame.add(view);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
-                return frame;
-        }
+        return frame;
+    }
 
-        public AppBuilder addLoginView() {
-                loginViewModel = new LoginViewModel();
-                loginView = new LoginView(loginViewModel);
-                cardPanel.add(loginView, loginView.getViewName());
-                return this;
-        }
+    public AppBuilder addLoginView() {
+        loginViewModel = new LoginViewModel();
+        loginView = new LoginView(loginViewModel);
+        cardPanel.add(loginView, loginView.getViewName());
+        return this;
+    }
 
-        public AppBuilder addLoggedInView() {
-                loggedInViewModel = new LoggedInViewModel();
-                loggedInView = new LoggedInView(loggedInViewModel);
-                cardPanel.add(loggedInView, loggedInView.getViewName());
-                return this;
-        }
+    public AppBuilder addLoggedInView() {
+        loggedInViewModel = new LoggedInViewModel();
+        loggedInView = new LoggedInView(loggedInViewModel);
+        cardPanel.add(loggedInView, loggedInView.getViewName());
+        return this;
+    }
 
-        public AppBuilder addSignupUseCase() {
-                final SignupOutputBoundary signupOutputBoundary = new SignupPresenter(viewManagerModel,
-                                signupViewModel, loginViewModel);
-                final SignupInputBoundary userSignupInteractor = new SignupInteractor(
-                                userDataAccessObject, signupOutputBoundary, userFactory);
+    public AppBuilder addSignupUseCase() {
+        final SignupOutputBoundary signupOutputBoundary = new SignupPresenter(viewManagerModel,
+                signupViewModel, loginViewModel);
+        final SignupInputBoundary userSignupInteractor = new SignupInteractor(
+                userDataAccessObject, signupOutputBoundary, userFactory);
 
-                SignupController controller = new SignupController(userSignupInteractor);
-                signupView.setSignupController(controller);
-                return this;
-        }
+        SignupController controller = new SignupController(userSignupInteractor);
+        signupView.setSignupController(controller);
+        return this;
+    }
 
-        public AppBuilder addLoginUseCase() {
-                final LoginOutputBoundary loginOutputBoundary = new LoginPresenter(viewManagerModel,
-                                loggedInViewModel, loginViewModel);
-                final LoginInputBoundary loginInteractor = new LoginInteractor(
-                                userDataAccessObject, loginOutputBoundary);
+    public AppBuilder addLoginUseCase() {
+        final LoginOutputBoundary loginOutputBoundary = new LoginPresenter(viewManagerModel,
+                loggedInViewModel, loginViewModel);
+        final LoginInputBoundary loginInteractor = new LoginInteractor(
+                userDataAccessObject, loginOutputBoundary);
 
-                LoginController loginController = new LoginController(loginInteractor);
-                loginView.setLoginController(loginController);
-                return this;
-        }
+        LoginController loginController = new LoginController(loginInteractor);
+        loginView.setLoginController(loginController);
+        return this;
+    }
 
-        public AppBuilder addChangePasswordUseCase() {
-                final ChangePasswordOutputBoundary changePasswordOutputBoundary = new ChangePasswordPresenter(
-                                viewManagerModel,
-                                loggedInViewModel);
+    public AppBuilder addChangePasswordUseCase() {
+        final ChangePasswordOutputBoundary changePasswordOutputBoundary = new ChangePasswordPresenter(
+                viewManagerModel,
+                loggedInViewModel);
 
-                final ChangePasswordInputBoundary changePasswordInteractor = new ChangePasswordInteractor(
-                                userDataAccessObject,
-                                changePasswordOutputBoundary, userFactory);
+        final ChangePasswordInputBoundary changePasswordInteractor = new ChangePasswordInteractor(
+                userDataAccessObject,
+                changePasswordOutputBoundary, userFactory);
 
-                ChangePasswordController changePasswordController = new ChangePasswordController(
-                                changePasswordInteractor);
-                loggedInView.setChangePasswordController(changePasswordController);
-                return this;
-        }
+        ChangePasswordController changePasswordController = new ChangePasswordController(
+                changePasswordInteractor);
+        loggedInView.setChangePasswordController(changePasswordController);
+        return this;
+    }
 
-        public AppBuilder addHomePageView(NavigationController controller) {
-                homePage = new HomePage(controller);
-                cardPanel.add(homePage, homePage.getViewName());
-                return this;
-        }
+    // public AppBuilder addHomePageView(NavigationController controller) {
+    // homePage = new HomePage(controller);
+    // cardPanel.add(homePage, homePage.getViewName());
+    // return this;
+    // }
 
-        /**
-         * Adds the Logout Use Case to the application.
-         * 
-         * @return this builder
-         */
-        public AppBuilder addLogoutUseCase() {
-                final LogoutOutputBoundary logoutOutputBoundary = new LogoutPresenter(viewManagerModel,
-                                loggedInViewModel, loginViewModel);
+    /**
+     * Adds the Logout Use Case to the application.
+     * 
+     * @return this builder
+     */
+    public AppBuilder addLogoutUseCase() {
+        final LogoutOutputBoundary logoutOutputBoundary = new LogoutPresenter(viewManagerModel,
+                loggedInViewModel, loginViewModel);
 
-                final LogoutInputBoundary logoutInteractor = new LogoutInteractor(userDataAccessObject,
-                                logoutOutputBoundary);
+        final LogoutInputBoundary logoutInteractor = new LogoutInteractor(userDataAccessObject,
+                logoutOutputBoundary);
 
-                final LogoutController logoutController = new LogoutController(logoutInteractor);
-                loggedInView.setLogoutController(logoutController);
-                return this;
-        }
+        final LogoutController logoutController = new LogoutController(logoutInteractor);
+        loggedInView.setLogoutController(logoutController);
+        return this;
+    }
 
-        public JFrame build() {
-                final JFrame application = new JFrame("User Login Example");
-                application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-                application.setSize(1000, 700);
-                application.setLocationRelativeTo(null);
-                application.add(cardPanel);
+    public JFrame build() {
+        final JFrame application = new JFrame("User Login Example");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        application.setSize(1000, 700);
+        application.setLocationRelativeTo(null);
 
-                viewManagerModel.setState(homePage.getViewName());
-                viewManagerModel.firePropertyChange();
+        JPanel cardContainer = new JPanel(new BorderLayout());
 
-                application.setVisible(true);
+        cardContainer.add(sidebarView, BorderLayout.WEST);
+        cardContainer.add(cardPanel, BorderLayout.CENTER);
+        // this.cardPanel.setLayout(new BorderLayout());
+        // this.cardPanel.add(cardContainer);
 
-                return application;
-        }
+        application.add(cardContainer);
+
+        viewManagerModel.setState(reviewFlashCardsView.getViewName());
+        viewManagerModel.firePropertyChange();
+
+        application.setVisible(true);
+
+        return application;
+    }
 
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,28 +1,17 @@
 package app;
 
-import interface_adapter.ViewManagerModel;
-import interface_adapter.navigation.NavigationController;
-import interface_adapter.navigation.NavigationPresenter;
-import use_case.navigation.NavigationInputBoundary;
-import use_case.navigation.NavigationInteractor;
-import use_case.navigation.NavigationOutputBoundary;
-import view.mainFrame;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Create ViewManagerModel
-        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        AppBuilder appBuilder = new AppBuilder();
+        JFrame application = appBuilder.addSidebar().addReviewFlashCardsView()
+                .build();
 
-        // Create Presenter
-        NavigationOutputBoundary presenter = new NavigationPresenter(viewManagerModel);
+        appBuilder.buildCreateFlashcardUI();
 
-        // Create Interactor
-        NavigationInputBoundary interactor = new NavigationInteractor(presenter);
-
-        // Create Controller
-        NavigationController navigationController = new NavigationController(interactor);
-
-        // Create and show mainFrame
-        new mainFrame(navigationController, viewManagerModel);
+        application.pack();
+        application.setLocationRelativeTo(null);
+        application.setVisible(true);
     }
 }
